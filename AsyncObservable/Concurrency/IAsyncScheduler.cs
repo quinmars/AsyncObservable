9396fa2a2cee88f26e14fb86c6c3b9916ae9e9a3
Peter Wehrfritz <peter.wehrfritz@mailbox.org>
2018-11-25 23:02:04 +0100

    Add preliminary interface for the schedulers.

---
+++ AsyncObservable/Concurrency/IAsyncScheduler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Quinmars.AsyncObservable
+{
+    public interface IAsyncScheduler
+    {
+        /*
+         * Note: This is a preliminary design for the
+         * scheduler interface. There remains the
+         * question how it will be possible to observe
+         * on a given thread/synchronisation context.
+         */
+        Task Delay(TimeSpan ts, CancellationToken ca);
+        DateTimeOffset Now { get; }
+    }
+}

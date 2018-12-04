144abdc9ac89249b76c8f4599681fcf625c30aba
Peter Wehrfritz <peter.wehrfritz@mailbox.org>
2018-12-04 23:46:04 +0100

    Add test extensions

---
+++ AsyncObservable.Tests/TestExtensions.cs
@@ -0,0 +1,48 @@
+﻿using Quinmars.AsyncObservable;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    static class TestExtensions
+    {
+        public static IAsyncObservable<int> ProduceSlowly(this IAsyncScheduler scheduler, params TimeSpan[] delays)
+        {
+            return AsyncObservable.Range(0, delays.Length)
+                .Select(async (i, ca) =>
+                {
+                    await scheduler.Delay(delays[i], ca);
+                    return i;
+                });
+        }
+
+        public static async Task<IList<(T, TimeSpan)>> ConsumeFast<T>(this IAsyncObservable<T> obs, TestAsyncScheduler scheduler)
+        {
+            var list = new List<(T, TimeSpan)>();
+
+            await obs.SubscribeAsync(i =>
+            {
+                list.Add((i, scheduler.EllapsedTime));
+            });
+
+            return list;
+        }
+
+        public static async Task<IList<(T, TimeSpan)>> ConsumeSlowly<T>(this IAsyncObservable<T> obs, TestAsyncScheduler scheduler, params TimeSpan[] delays)
+        {
+            var list = new List<(T, TimeSpan)>();
+            int index = 0;
+
+            await obs.SubscribeAsync(async i =>
+            {
+                await scheduler.Delay(delays[index], default);
+                list.Add((i, scheduler.EllapsedTime));
+                index++;
+            });
+
+            return list;
+        }
+    }
+}
---
+++ AsyncObservable.Tests/TestExtensionsTests.cs
@@ -0,0 +1,94 @@
+﻿using FluentAssertions;
+using FluentAssertions.Extensions;
+using Quinmars.AsyncObservable;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Tests
+{
+    public class TestExtensionsTests
+    {
+        [Fact]
+        public async Task ProduceSlowly()
+        {
+            var scheduler = new TestAsyncScheduler();
+
+            var result = await scheduler.RunAsync(async () =>
+            {
+                return await scheduler.ProduceSlowly(
+                        10.Seconds(),
+                        10.Seconds(),
+                        10.Seconds(),
+                        10.Seconds(),
+                        10.Seconds())
+                    .ConsumeFast(scheduler);
+            });
+
+            result
+                .Should().Equal(
+                (0, 10.Seconds()),
+                (1, 20.Seconds()),
+                (2, 30.Seconds()),
+                (3, 40.Seconds()),
+                (4, 50.Seconds()));
+        }
+
+        [Fact]
+        public async Task ConsumeSlowly()
+        {
+            var scheduler = new TestAsyncScheduler();
+
+            var result = await scheduler.RunAsync(async () =>
+            {
+                return await AsyncObservable.Range(0, 5)
+                    .ConsumeSlowly(scheduler,
+                        10.Seconds(),
+                        10.Seconds(),
+                        10.Seconds(),
+                        10.Seconds(),
+                        10.Seconds());
+            });
+
+            result
+                .Should().Equal(
+                (0, 10.Seconds()),
+                (1, 20.Seconds()),
+                (2, 30.Seconds()),
+                (3, 40.Seconds()),
+                (4, 50.Seconds()));
+        }
+
+        [Fact]
+        public async Task ProduceConsumeSlowly()
+        {
+            var scheduler = new TestAsyncScheduler();
+
+            var result = await scheduler.RunAsync(async () =>
+            {
+                return await scheduler.ProduceSlowly(
+                        10.Seconds(),
+                        10.Seconds(),
+                        10.Seconds(),
+                        10.Seconds(),
+                        10.Seconds())
+                    .ConsumeSlowly(scheduler,
+                        10.Seconds(),
+                        10.Seconds(),
+                        10.Seconds(),
+                        10.Seconds(),
+                        10.Seconds());
+            });
+
+            result
+                .Should().Equal(
+                (0, 20.Seconds()),
+                (1, 40.Seconds()),
+                (2, 60.Seconds()),
+                (3, 80.Seconds()),
+                (4, 100.Seconds()));
+        }
+    }
+}
